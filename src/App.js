fef0fc6f543b4085130b2fd1fdc96872f0703fe1
unknown <andriescuioana@gmail.com>
2020-05-15 15:07:45 +0200

    base structure and styling

--- src/App.js
+++ src/App.js
@@ -4,7 +4,20 @@ import './App.css';
 function App() {
   return (
     <div className="app">
-      Hello
+      <div className="browser">
+        <div className="tabs">
+          <div className="tab">
+            <a>Home</a>
+          </div>
+          <div className="tab">
+            <a>About</a>
+          </div>
+          <div className="tab">
+            <a>Features</a>
+          </div>
+        </div>
+        <div className="viewport">Pages Go Here</div>
+      </div>
     </div>
   );
 }
