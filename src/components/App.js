76fa92208127e7d961ecb5d38acf9b9f382e3d61
unknown <andriescuioana@gmail.com>
2020-05-16 16:24:55 +0200

    refactoring

--- src/components/App.js
+++ src/components/App.js
@@ -1,39 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, NavLink } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import '../App.css';
-import Tab from './Tab';
-import Home from '../pages/Home';
-import About from '../pages/About';
-import Features from '../pages/Features';
+import Header from './Header';
+import Routes from '../Routes';
 
 function App() {
   return (
     <Router>
       <div className="app">
         <div className="browser">
-          <div className="tabs">
-            <Tab>
-              <NavLink to="/" exact activeClassName="is-active">Home</NavLink>
-            </Tab>
-            <Tab>
-              <NavLink to="/about" activeClassName="is-active">About</NavLink>
-            </Tab>
-            <Tab>
-              <NavLink to="features" activeClassName="is-active">Features</NavLink>
-            </Tab>
-          </div>
+          <Header />
           <div className="viewport">
-            <Switch>
-              <Route path="/about">
-                <About />
-              </Route>
-              <Route path="/features">
-                <Features />
-              </Route>
-              <Route path="/" exact>
-                <Home />
-              </Route>
-            </Switch>
+            <Routes />
           </div>
         </div>
       </div>
